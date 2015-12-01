32c80eaf4c9cb8f4fc92a2c33edbe3ea8d809e07
Pavandeep <puddupakkam@gmail.com>
2015-12-01 14:31:07 +1100

    Updated the functions.

--- src/test/java/library/utils/WebDriverUtils.java
+++ src/test/java/library/utils/WebDriverUtils.java
@@ -18,6 +18,8 @@
 package library.utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.log4testng.Logger;
@@ -56,25 +58,36 @@ public class WebDriverUtils {
     public WebDriver createWebDriver(String browserName, String remoteWebDriverUrl) throws MalformedURLException {
         LOGGER.debug("Creating [" + browserName + "] WebDriver");
         WebDriver webDriver;
-        if (webDriverCache.containsKey(browserName)) {
-            webDriver = webDriverCache.get(browserName);
-        } else {
-            if (BROWSER_FIREFOX.contains(browserName) || BROWSER_HTML_UNIT.contains(browserName) || BROWSER_IE.contains
-                    (browserName) || BROWSER_CHROME.contains(browserName)) {
-                webDriver = getWebDriver(remoteWebDriverUrl, browserName);
-                browserName = BROWSER_REMOTE.contains(browserName) ? BROWSER_REMOTE + " - " + remoteBrowserName(
-                        (RemoteWebDriver) webDriver) : remoteBrowserName((RemoteWebDriver) webDriver);
-            } else if (BROWSER_SAUCELABS.contains(browserName)) {
-                DesiredCapabilities caps = internetExplorer();
-                caps.setCapability("platform", "Windows 7");
-                caps.setCapability("version", "11");
-                webDriver = new RemoteWebDriver(new URL(remoteWebDriverUrl), caps);
-                browserName = BROWSER_REMOTE + " - " + remoteBrowserName((RemoteWebDriver) webDriver);
-            } else {
-                throw new IllegalArgumentException("Unable to determine the driver for [" + browserName + "]");
+        if (remoteWebDriverUrl == null) {
+            webDriver = null;
+            if (browserName.contains("Firefox")) {
+                webDriver = new FirefoxDriver();
             }
-            LOGGER.debug("Caching [" + browserName + "] WebDriver");
-            webDriverCache.put(browserName, webDriver);
+            if (browserName.contains("Chrome")) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\chromedriver.exe");
+                webDriver = new ChromeDriver();
+            }
+        } else {
+            if (webDriverCache.containsKey(browserName)) {
+                webDriver = webDriverCache.get(browserName);
+            } else {
+                if (BROWSER_FIREFOX.contains(browserName) || BROWSER_HTML_UNIT.contains(browserName) || BROWSER_IE.contains
+                        (browserName) || BROWSER_CHROME.contains(browserName)) {
+                    webDriver = getWebDriver(remoteWebDriverUrl, browserName);
+                    browserName = BROWSER_REMOTE.contains(browserName) ? BROWSER_REMOTE + " - " + remoteBrowserName(
+                            (RemoteWebDriver) webDriver) : remoteBrowserName((RemoteWebDriver) webDriver);
+                } else if (BROWSER_SAUCELABS.contains(browserName)) {
+                    DesiredCapabilities caps = internetExplorer();
+                    caps.setCapability("platform", "Windows 7");
+                    caps.setCapability("version", "11");
+                    webDriver = new RemoteWebDriver(new URL(remoteWebDriverUrl), caps);
+                    browserName = BROWSER_REMOTE + " - " + remoteBrowserName((RemoteWebDriver) webDriver);
+                } else {
+                    throw new IllegalArgumentException("Unable to determine the driver for [" + browserName + "]");
+                }
+                LOGGER.debug("Caching [" + browserName + "] WebDriver");
+                webDriverCache.put(browserName, webDriver);
+            }
         }
         return webDriver;
     }
